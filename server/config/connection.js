9f4b292c852b52369c9d16960a89d5d171159260
Tyrion Lannister <win.pomerantz@gmail.com>
2024-06-01 13:05:07 -0700

    altering config for render/apollo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,30 @@
-const mongoose = require('mongoose');
-require("dotenv").config();
+// const mongoose = require('mongoose');
+// require("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
+// module.exports = mongoose.connection;
+
+
+const mongoose = require('mongoose');
+
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to the database');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(`Mongoose connection error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected from the database');
+});
 
 module.exports = mongoose.connection;
